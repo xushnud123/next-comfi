79342641d069dc8c91615c165b7388df4a954ace
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-02-18 19:18:31 +0500

    sharp

--- src/helpers/analytic.ts
+++ src/helpers/analytic.ts
@@ -3,7 +3,7 @@ interface Events {}
 function analytic(event: string, events: Events) {
   if (process.env.NODE_ENV === 'production') {
     // @ts-ignore
-    window?.dataLayer.push({ event, ...events });
+    // window?.dataLayer.push({ event, ...events });
   }
 }
 
